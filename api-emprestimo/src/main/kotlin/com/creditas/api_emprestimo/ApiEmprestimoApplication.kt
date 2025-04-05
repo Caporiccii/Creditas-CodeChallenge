10c427653be4982389f7ddfb33f3377bcebe40dc
Caporiccii <dmarco792@gmail.com>
2025-04-05 19:15:34 -0300

    [first commit] Create new project.

---
+++ api-emprestimo/src/main/kotlin/com/creditas/api_emprestimo/ApiEmprestimoApplication.kt
@@ -0,0 +1,11 @@
+package com.creditas.api_emprestimo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiEmprestimoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiEmprestimoApplication>(*args)
+}
---
+++ api-emprestimo/src/test/kotlin/com/creditas/api_emprestimo/ApiEmprestimoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.creditas.api_emprestimo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiEmprestimoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
